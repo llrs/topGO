078f638013c9013ce8dfc2855f517b6d2cd6aadd
H Bengtsson <hb@aroma-project.org>
2016-02-03 16:26:36 +0000

    Fix partial argument matching
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/topGO@113229 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/topGOfunctions.R
+++ R/topGOfunctions.R
@@ -1,5 +1,5 @@
 #################### misc functions ####################
-## 
+##
 ##
 ##
 ##
@@ -37,7 +37,7 @@ groupGOTerms <- function(where) {
 inverseList <- function(l) {
   rId <- unlist(l, use.names = FALSE)
   lId <- rep(names(l), sapply(l, length))
-  
+
   return(split(lId, rId))
 }
 
@@ -67,14 +67,14 @@ if(!isGeneric("printGenes"))
 setMethod("printGenes",
           signature(object = "topGOdata", whichTerms = "character", file = "missing"),
           function(object, whichTerms, chip, numChar = 100, simplify = TRUE,
-                   geneCutOff = 50, pvalCutOff) { 
-            
+                   geneCutOff = 50, pvalCutOff) {
+
             term.genes <- genesInTerm(object, whichTerms)
             all.genes <- character()
             lapply(term.genes, function(x) all.genes <<- union(x, all.genes))
 
             chip <- sub(".db$", "", chip)
-            
+
             LL.lib <- get(paste(chip, "ENTREZID", sep = ""))
             Sym.lib <- get(paste(chip, "SYMBOL", sep = ""))
             GNAME.lib <- get(paste(chip, "GENENAME", sep = ""))
@@ -86,7 +86,7 @@ setMethod("printGenes",
 
             retList <- vector("list", length(whichTerms))
             names(retList) <- whichTerms
-            
+
             for(gt in whichTerms) {
               affID <- term.genes[[gt]]
 
@@ -100,21 +100,21 @@ setMethod("printGenes",
 
               ## we restrict the output to the number of genes
               length(affID) <- min(length(affID), geneCutOff)
-              
+
               ## if there are no genes, there is nothing to print
               if(length(affID) == 0) {
                 cat("\n\t No genes over the cutOff for:", gt, "\n")
                 next
               }
-              
+
               genesNames <- sapply(mget(affID, envir = GNAME.lib),
                                    function(x) return(x[1]))
               genesNames <- paste(substr(genesNames, 1, numChar),
                                   ifelse(nchar(genesNames) > numChar, "...", ""), sep = "")
-              
+
               retList[[gt]] <- cbind("Chip ID" = affID, probeMapping[affID, ], "Gene name" = genesNames,
-                                     "raw p-value" = format.pval(pval[affID], dig = 3, eps = 1e-30))
-            }            
+                                     "raw p-value" = format.pval(pval[affID], digits = 3, eps = 1e-30))
+            }
 
             ## if we have only one GO term we return just the data.frame
             if(simplify && length(whichTerms) == 1)
@@ -127,7 +127,7 @@ setMethod("printGenes",
 setMethod("printGenes",
           signature(object = "topGOdata", whichTerms = "character", file = "character"),
           ## numChar = "integer"),
-          function(object, whichTerms, file, oneFile = FALSE, ...) { 
+          function(object, whichTerms, file, oneFile = FALSE, ...) {
 
             infoList <- printGenes(object, whichTerms, ...)
 
@@ -137,7 +137,7 @@ setMethod("printGenes",
                           col.names = colnames(infoList), row.names = 1:nrow(infoList))
               return()
             }
-            
+
             if(oneFile) {
               fAppend <- FALSE
               for(gt in whichTerms) {
@@ -149,8 +149,8 @@ setMethod("printGenes",
                 return()
               }
             }
-            
-            for(gt in whichTerms) 
+
+            for(gt in whichTerms)
               write.table(infoList[[gt]], quote = TRUE, sep = ",", append = FALSE,
                           file = paste(paste(file, sub(":", "_", gt), sep = "_"), "csv", sep = "."),
                           col.names = colnames(infoList[[gt]]), row.names = 1:nrow(infoList[[gt]]))
@@ -164,11 +164,11 @@ setMethod("printGenes",
   qTerms <- paste(paste("'", whichTerms, "'", sep = ""), collapse = ",")
   retVal <- dbGetQuery(GO_dbconn(), paste("SELECT term, go_id FROM go_term WHERE ontology IN",
                                           "('", ontology, "') AND go_id IN (", qTerms, ");", sep = ""))
-  
+
   termsNames <- retVal$term
   names(termsNames) <- retVal$go_id
-  
-  if(!multipLines) 
+
+  if(!multipLines)
     shortNames <- paste(substr(termsNames, 1, numChar),
                         ifelse(nchar(termsNames) > numChar, '...', ''), sep = '')
   else
@@ -177,14 +177,14 @@ setMethod("printGenes",
                            a <- strwrap(x, numChar)
                            return(paste(a, sep = "", collapse = "\\\n"))
                          })
-  
+
   names(shortNames) <- names(termsNames)
   return(shortNames[whichTerms])
 }
 
 
 ## methodsSig contains a named vector of p-values for each run method
-.sigAllMethods <- function (methodsSig) 
+.sigAllMethods <- function (methodsSig)
 {
   names.index <- names(methodsSig[[1]])
   retval <- as.data.frame(lapply(methodsSig, function(x) x[names.index]))
@@ -210,26 +210,26 @@ setMethod("GenTable",
                    topNodes = 10, numChar = 40,
                    format.FUN = format.pval, decreasing = FALSE,
                    useLevels = FALSE) {
-            
+
             resList <- list(...)
-            
+
             ## first for the class of the elements in the list
             if(!all(sapply(resList, is, "topGOresult")))
               stop("Use: topGOdata, topGOresult_1, topGOresult_2, ..., \"parameters\".")
-            
+
             ## if no names were provided we name them
             if(is.null(names(resList)))
               names(resList) <- paste("result", 1:length(resList), sep = "")
 
             ## obtain the score from the objects
             resList <- lapply(resList, score)
-            
+
             ## order the scores and take care of the case in which only one result is provided
             ## in such case the orderBy and ranksOf parameters are ignored.
             if(length(resList) == 1) {
               orderBy <- ranksOf <- 1
               l <- data.frame(resList)
-              names(l) <- ifelse(is.null(names(resList)), "", names(resList)) 
+              names(l) <- ifelse(is.null(names(resList)), "", names(resList))
             } else {
               l <- .sigAllMethods(resList)
             }
@@ -238,25 +238,25 @@ setMethod("GenTable",
             l <- l[index, , drop = FALSE]
 
             if(decreasing)
-              rr <- rank(-l[, ranksOf], ties = "first")
+              rr <- rank(-l[, ranksOf], ties.method = "first")
             else
-              rr <- rank(l[, ranksOf], ties = "first")
+              rr <- rank(l[, ranksOf], ties.method = "first")
 
             whichTerms <- rownames(l)[1:topNodes]
             l <- l[whichTerms, , drop = FALSE]
             rr <- as.integer(rr[1:topNodes])
 
             shortNames <- .getTermsDefinition(whichTerms, ontology(object), numChar = numChar)
-            
+
             infoMat <- data.frame('GO ID' = whichTerms, 'Term' = shortNames, stringsAsFactors = FALSE)
-            
+
             ## put the levels of the GO
             if(useLevels) {
               nodeLevel <- buildLevels(graph(object), leafs2root = TRUE)
               nodeLevel <- unlist(mget(whichTerms, envir = nodeLevel$nodes2level))
               infoMat <- data.frame(infoMat, Level = as.integer(nodeLevel))
             }
-            
+
             annoStat <- termStat(object, whichTerms)
 
             ## if orderBy == ranksOf then there is no need to put the ranks
@@ -273,11 +273,11 @@ setMethod("GenTable",
                                     apply(l, 2, format.FUN, dig = 2, eps = 1e-30),
                                     check.names = FALSE, stringsAsFactors = FALSE)
             }
-            
+
             ##rownames(infoMat) <- whichTerms
             rownames(infoMat) <- 1:length(whichTerms)
-            
-            return(infoMat)            
+
+            return(infoMat)
           })
 
 
@@ -292,11 +292,11 @@ setMethod("GenTable",
 ##                     topNodes = "integer",
 ##                     numChar = "integer"),
 ##           function(object, resList, orderBy, ranksOf, topNodes = 10, no.char = 40) {
-            
 
-## = infoMat, 
+
+## = infoMat,
 ##                         pval.tab = xtable.matrix(infoMat, caption = 'GO terms p-value', label = 'tab:GOinfo')))
-          
+
 
 
 
@@ -309,71 +309,71 @@ getPvalues <- function(edata, classlabel, test = "t",
                        genesID = NULL,
                        correction = c("none", "Bonferroni", "Holm", "Hochberg",
                          "SidakSS", "SidakSD", "BH", "BY")[8]) {
-  
+
   require('multtest') || stop('package multtest is required')
 
   ## restrict the dataset
   if(!is.null(genesID))
     edata <- edata[intersect(genesID, rownames(edata)), ]
   genesID <- rownames(edata)
-  
+
   t.stats <- mt.teststat(edata, classlabel = classlabel, test = test)
 
-  if (alternative == "less") 
+  if (alternative == "less")
     p.values <- pt(t.stats, df = length(classlabel) - 2)
-  else if (alternative == "greater") 
-    p.values <- pt(t.stats, df = length(classlabel) - 2, lower = FALSE)
-  else 
+  else if (alternative == "greater")
+    p.values <- pt(t.stats, df = length(classlabel) - 2, lower.tail = FALSE)
+  else
     p.values <- 2 * pt(-abs(t.stats), df = length(classlabel) - 2)
-  
+
   if(correction != "none") {
     p.values <- mt.rawp2adjp(p.values, correction)
     p.values <- p.values$adjp[order(p.values$index), correction]
   }
   names(p.values) <- genesID
-  
+
   return(p.values)
 }
 
 ################################################################################
 
-## a <= b 
+## a <= b
 .sigRatio.ratio <- function(a, b, tolerance = 1e-50) {
-  
+
   ## if a and b are almost equal we return 2
   if(identical(all.equal(a, b, tolerance = tolerance), TRUE))
     return(2)
-  
+
   ## we want to compute b / a, thus we must take care for a = 0
   if(identical(all.equal(a, 0, tolerance = tolerance), TRUE))
     return(abs(b / (a + tolerance)))
-  
+
   return(abs(b / a))
 }
 
 .sigRatio.log <- function(a, b, tolerance = 1e-50) {
-  
+
   ## if a and b are almost equal we return 2
   if(identical(all.equal(a, b, tolerance = tolerance), TRUE))
     return(2)
-  
+
   ## we want to compute log(a) / log(b), thus we must take care for b = 1
   if(identical(all.equal(log10(b), 0, tolerance = tolerance), TRUE))
     return(abs(log10(a) / tolerance))
-  
+
   return(abs(log10(a) / log10(b)))
 }
 
-## a <= b 
+## a <= b
 .sigRatio.01 <- function(a, b, tolerance = 1e-50) {
-  
+
   ## if a and b are almost equal we return 2
   if(identical(all.equal(a, b, tolerance = tolerance), TRUE))
     return(2)
-  
+
   if(a < b)
     return(1e50)
-  
+
   return(2)
 }
 
@@ -410,10 +410,10 @@ combineResults <- function(..., method = c("gmean", "mean", "median", "min", "ma
   ## first for the class of the elements in the list
   if((length(resList) < 2) || !all(sapply(resList, is, "topGOresult")))
     stop("Use: topGOresult_1, topGOresult_2, ..., method = \"mean\"")
-  
+
   combMethod <- match.arg(method)
 
-  retVal <- resList[[1]] 
+  retVal <- resList[[1]]
 
   ## update the infos
   description(retVal) <- paste(description(retVal),
@@ -423,13 +423,13 @@ combineResults <- function(..., method = c("gmean", "mean", "median", "min", "ma
                                sep = "\n")
   testName(retVal) <- paste(unique(sapply(resList, testName)), collapse = ", ")
   algorithm(retVal) <- "ensemble"
-    
+
   ## get the list of GOs
   nn <- names(score(retVal))
   ## obtain the score from the objects
   resList <- do.call(cbind, lapply(resList, score, whichGO = nn))
 
-  newRes <- switch(combMethod, 
+  newRes <- switch(combMethod,
                    mean = rowMeans(resList),
                    median = rowMedians(resList),
                    min = rowMin(resList),
