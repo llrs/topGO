debf998e687e5554b199d8bc31d3c74e3489c454
H Bengtsson <hb@aroma-project.org>
2016-02-03 16:34:36 +0000

    globaltest:::p.value() -> globaltest::p.value()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/topGO@113232 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/topGOtests.R
+++ R/topGOtests.R
@@ -2,12 +2,12 @@
 
 if(!isGeneric("GOFisherTest"))
   setGeneric("GOFisherTest", function(object) standardGeneric("GOFisherTest"))
-                               
+
 setMethod("GOFisherTest", "classicCount",
           function(object) {
 
             contMat <- contTable(object)
-            
+
             if(all(contMat == 0))
               p.value <- 1
             else
@@ -20,7 +20,7 @@ setMethod("GOFisherTest", "classicCount",
 
 if(!isGeneric("GOKSTest"))
   setGeneric("GOKSTest", function(object) standardGeneric("GOKSTest"))
-                               
+
 setMethod("GOKSTest", "classicScore",
           function(object) {
 
@@ -33,7 +33,7 @@ setMethod("GOKSTest", "classicScore",
 
             x.a <- rankMembers(object)
             ## x.b <- setdiff(1:N, x.a)
-            
+
             return(ks.test(x.a, seq_len(N)[-x.a], alternative = "greater")$p.value)
           })
 
@@ -41,21 +41,21 @@ setMethod("GOKSTest", "classicScore",
 
 if(!isGeneric("GOtTest"))
   setGeneric("GOtTest", function(object) standardGeneric("GOtTest"))
-                               
+
 setMethod("GOtTest", "classicScore",
           function(object) {
 
-            ## we should try to do this smarter!!! 
+            ## we should try to do this smarter!!!
             var.equal <- FALSE
             ## check if there are any parameters set
             if(length(testStatPar(object)) != 0)
               if("var.equal" %in% names(testStatPar(object)))
                 var.equal <- testStatPar(object)[["var.equal"]]
-                                        
+
             nG <- numMembers(object)
             nNotG <- numAllMembers(object) - nG
-            
-            ## if the group/complementary is empty 
+
+            ## if the group/complementary is empty
             if(nNotG == 0 || nG == 0)
               return(1)
 
@@ -72,10 +72,10 @@ setMethod("GOtTest", "classicScore",
               .stat <- ifelse(aa, sum(x.NotG >= x.G), sum(x.NotG <= x.G))
               p.value <- (.stat + 1) / (nG + nNotG + 1)
             }
-            else 
+            else
               p.value <- t.test(x = x.G, y = x.NotG, var.equal = var.equal,
                                 alternative = ifelse(aa, "greater", "less"))$p.value
-            
+
             return(p.value)
           })
 
@@ -83,14 +83,14 @@ setMethod("GOtTest", "classicScore",
 
 if(!isGeneric("GOglobalTest"))
   setGeneric("GOglobalTest", function(object) standardGeneric("GOglobalTest"))
-                               
+
 setMethod("GOglobalTest", "classicExpr",
           function(object) {
-            
+
             if(numMembers(object) == 0)
               return(1)
 
-            return(globaltest:::p.value(globaltest:::globaltest(X = membersExpr(object), Y = pType(object))))
+            return(globaltest::p.value(globaltest:::globaltest(X = membersExpr(object), Y = pType(object))))
           })
 
 
@@ -102,7 +102,7 @@ permSumStats <- function(object, N) {
 
   ## the available GOs and their size
   goSize <- sort(unique(countGenesInTerm(object)))
-  
+
   ## remove the GO sizes which are 60% or more of the max size
   goSize <- goSize[goSize / max(goSize) < .6]
   maxSample <- goSize[length(goSize)]
@@ -114,7 +114,7 @@ permSumStats <- function(object, N) {
   assign(".PERMSUM.MAT", sapply(1:N, function(x) cumsum(sample(scoreVec, maxSample))[goSize]), .GlobalEnv)
 }
 
-## same as above just doesn't remove any term 
+## same as above just doesn't remove any term
 permSumStats.all <- function(object, N) {
 
   ## the gene scores
@@ -132,41 +132,41 @@ permSumStats.all <- function(object, N) {
 }
 
 
-## Category test based on score sums 
+## Category test based on score sums
 if(!isGeneric("GOSumTest"))
   setGeneric("GOSumTest", function(object) standardGeneric("GOSumTest"))
-                               
+
 setMethod("GOSumTest", "classicScore",
           function(object) {
 
             nG <- numMembers(object)
             if(nG == 0) return(1)
             N <- ncol(.PERMSUM.MAT)
-            
+
             ## compute the group sum (statistic)
             sObserved <- sum(membersScore(object))
-            
+
             ## check to see if we have a precomputed the permutations for this group size
             isPermuted <- .PERMSUM.LOOKUP[nG]
-            if(isPermuted > 0) 
+            if(isPermuted > 0)
               ## obtain the vector of already permuted values and compute the p-value
               return((sum(.PERMSUM.MAT[isPermuted, ] >= sObserved) + 1) / (N + 1))
-            
+
             ## at this point we don't have permuations for the current group size
             numScore <- numAllMembers(object)  ##  length(scoreVec)
             scoreVec <- allScore(object, FALSE)
 
             ##cat("\rComputing", N, "permutations for group size:", nG, "...\n")
             ##flush.console()
-            
-            if(nG > numScore / 2) { 
+
+            if(nG > numScore / 2) {
               ## we simply compute the sum statistic
               permSums <- sapply(1:N, function(x) sum(sample(scoreVec, nG)))
             }
             else { ## we try to do a bit of optimisation using the cumsum function
-              chunkIndex <- seq.int(from = nG, to = numScore, by = nG)   
+              chunkIndex <- seq.int(from = nG, to = numScore, by = nG)
               cycles <- N %/% (length(chunkIndex) - 1) + 1
-              
+
               permSums <-  unlist(lapply(1:cycles, function(y) {
                 x <- cumsum(scoreVec[.Internal(sample(numScore, numScore, FALSE, NULL))])[chunkIndex]
                 return(x[-1] - x[-length(x)])
@@ -174,7 +174,7 @@ setMethod("GOSumTest", "classicScore",
 
               length(permSums) <- N
             }
-            
+
             return((sum(permSums >= sObserved) + 1) / (N + 1))
           })
 
@@ -198,10 +198,10 @@ setMethod("GOKSTiesTest", "classicScore",
               numPerm <- 50L
             if(is.null(FUN))
               FUN <- "max"
-            
-            ## get all the scores and take care of their order 
+
+            ## get all the scores and take care of their order
             a.s <- allScore(object)
-            ## get the members index 
+            ## get the members index
             mem.index <- rankMembers(object)
             seqN <- seq_len(N)
             resP <- numeric(numPerm)
@@ -210,7 +210,7 @@ setMethod("GOKSTiesTest", "classicScore",
               x.a <- sort.list(order(a.s, sample(N), decreasing = scoreOrder(object)))[mem.index]
               resP[i] <- ks.test(x.a, seqN[-x.a], alternative = "greater")$p.value
             }
-            
+
             ## aggregate the resulting p-values and return
             return(get(FUN)(resP))
           })
